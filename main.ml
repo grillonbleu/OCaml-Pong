19b1697254851cc940fdcf14dd0eb86b75d51ff9
François Moreau <moreauf@sofad.qc.ca>
2013-05-21 14:02:30 -0400

    Doc et FPS maximum

--- main.ml
+++ main.ml
@@ -3,29 +3,32 @@
  *
  *  François Moreau
  *  mai 2013 
- *  TODO Instructions pour quitter, pause et souris
  *)
 
 #load "graphics.cma";;
 #load "unix.cma";; 
 
 (* Constantes *)
-
 let pi = acos (-. 1.);;
 let largeur_fenetre, hauteur_fenetre = 640, 480;;
+let maxfps = 40.;;
 
 
-(* Joueurs 
+(* État de l'application *)
+type etat = Splash | EnJeu | Pause | Quitter ;; 
+
+
+
+(* Les joueurs
+   Les fonctions des joueurs donnent accès à leurs
+   propriétés, protégées et partagées entre les fonctions
+   à partir d'une fermeture par instance.
    Joueur 1 est le joueur 
    Joueur 2 est l'adversaire *)
-
-
 type tjoueur = {
-    (* pour dessin et détection de collision*)
     (* http://stackoverflow.com/a/1936981 *)
     jr_passe_rect : 'a. (int -> int -> int -> int -> 'a) -> 'a ;
 
-    (* de -1. à 1., pour calcul d'angle de rebond  *)
     jr_hauteur_relative : int -> float ;
 
     jr_getyf : unit -> float ; 
@@ -35,20 +38,30 @@ type tjoueur = {
   };;
 
 let rec batiJoueur x y  = let 
-    largeur = 10 and
-    hauteur = 100 and 
-    x = x and
-    y = y and
+    largeur = 25 and
+    hauteur = 100 and
+    x = x and (* coordonnée à gauche de rectangle *)
+    y = y and (* coordonnée verticale au milieu du rectangle *)
     vitesse = 100. in {
 
+      (* Reçoit en paramètre une fonction qu'on appliquera aux paramètres :
+          - x,
+          - y, (du coin supérieur gauche), 
+          - largeur,
+          - hauteur
+         Est utilisé pour le rendu et la détection de collision avec la balle. *)
       jr_passe_rect = (fun f -> f 
-	  (int_of_float x) 
-	  ((int_of_float y) - (hauteur / 2))
-	  largeur
-	  hauteur );
+      (int_of_float x) 
+      ((int_of_float y) - (hauteur / 2))
+      largeur
+      hauteur );
 
+      (* Prend une coordonnée verticale et évalue sa différence par rapport au milieu du joueur.
+         Un résultat de -1.0 indique que le paramètre est sur la limite inférieure du joueur. 
+         Un résultat de 1.0 indique que le paramètre est sur la limite supérieure du joueur. 
+         Est utilisé pour le calcul d'angle de rebond.  *)
       jr_hauteur_relative = (fun i -> ((float_of_int i) -. y) /. 
-	                               ((float_of_int hauteur) /. 2.) ) ;
+                                   ((float_of_int hauteur) /. 2.) ) ;
 
       jr_getyf = (fun () -> y); 
 
@@ -63,8 +76,8 @@ let rec batiJoueur x y  = let
 
 (* La balle
    Les fonctions de la balle donnent accès à ses
-   propiétés, protégées et partagées entre les fonctions
-   à partir d'une fermeture commune. *)
+   propriétés, protégées et partagées entre les fonctions
+   à partir d'une fermeture. *)
 type tballe = {
     b_get_rayon : unit -> int ;
     b_getx : unit -> int ;
@@ -79,7 +92,6 @@ type tballe = {
     b_deplace : float -> tballe ;
     b_applique_sur_direction : (float -> float -> (float * float)) -> tballe ;
   };;
-
 
 let rec batiBalle x y vitessex vitessey =
   let
@@ -103,22 +115,22 @@ let rec batiBalle x y vitessex vitessey =
     b_set_pos = (fun nouvx nouvy ->
       batiBalle (float_of_int nouvx)
                 (float_of_int nouvy)
-	        vitessex
-	        vitessey ) ;
+            vitessex
+            vitessey ) ;
   
     (* Modifie l'angle de déplacement de la balle *)
     b_set_angle = (fun rad ->
       batiBalle x y 
-	((cos rad) *. vitesse)
-	((sin rad) *. vitesse) );
+    ((cos rad) *. vitesse)
+    ((sin rad) *. vitesse) );
 
     (* Calcule et applique la position dans laquelle la balle se trouve une 
        fois que "secondes_ecoulees" se sont écoulées. *)
     b_deplace = (fun secondes_ecoulees ->  
       batiBalle (x +. vitessex *. secondes_ecoulees)
-	        (y +. vitessey *. secondes_ecoulees)
-	        vitessex
-	        vitessey );
+            (y +. vitessey *. secondes_ecoulees)
+            vitessex
+            vitessey );
 
   (* Applique la fonction "f" au vecteur de vélocité représenté par 
      vitessex et vitessey *)
@@ -126,7 +138,6 @@ let rec batiBalle x y vitessex vitessey =
     let nouv_vx, nouv_vy = f vitessex vitessey in
     batiBalle x y nouv_vx nouv_vy );
 };;
-
 
 
 
@@ -138,40 +149,46 @@ let point_dans_rect px py rx ry rw rh =
   py < ry + rh &&
   py > ry ;;
 
-(*Opérateur pipe *)
+
+
+(* Opérateur pipe *)
 let (|>) x f = f x;;
 
 
-(* État de l'application *)
-type etat = Splash | EnJeu | Pause | Quitter ;; 
+(* Attente de moins d'une seconde *)
+let rec restart_on_EINTR f x =
+  try f x with Unix.Unix_error (Unix.EINTR, _, _) -> restart_on_EINTR f x;;
+
+let minisleep (sec: float) =
+    (ignore (restart_on_EINTR (Unix.select [] [] []) sec));;
 
 
 (* Gestion des entrées clavier et souris *)
 let entree etat balle joueur1 = match etat with
   | Splash ->
     (if (Graphics.key_pressed ()) then 
-	  match Graphics.read_key() with
-	  | 'q' -> Quitter, balle, joueur1
-	  | ' ' -> EnJeu,
-		   ((balle.b_set_pos (largeur_fenetre/2) (hauteur_fenetre/2)).b_set_angle 0.),
-		   joueur1
-	  | _ -> etat, balle, joueur1
+      match Graphics.read_key() with
+      | 'q' -> Quitter, balle, joueur1
+      | ' ' -> EnJeu,
+           ((balle.b_set_pos (largeur_fenetre/2) (hauteur_fenetre/2)).b_set_angle 0.),
+           joueur1
+      | _ -> etat, balle, joueur1
     else etat, balle, joueur1)
 
   | EnJeu -> let joueur1 = joueur1.jr_sety (snd (Graphics.mouse_pos())) in
     (if (Graphics.key_pressed ()) then 
-	  match Graphics.read_key() with
-	  | 'q' -> Quitter, balle, joueur1
-	  | ' ' -> Pause, balle, joueur1
-	  | _ -> etat, balle, joueur1
+      match Graphics.read_key() with
+      | 'q' -> Quitter, balle, joueur1
+      | ' ' -> Pause, balle, joueur1
+      | _ -> etat, balle, joueur1
     else etat, balle, joueur1)
 
   | Pause ->
     (if (Graphics.key_pressed ()) then 
-	  match Graphics.read_key() with
-	  | 'q' -> Quitter, balle, joueur1
-	  | ' ' -> EnJeu, balle, joueur1
-	  | _ -> etat, balle, joueur1
+      match Graphics.read_key() with
+      | 'q' -> Quitter, balle, joueur1
+      | ' ' -> EnJeu, balle, joueur1
+      | _ -> etat, balle, joueur1
     else etat, balle, joueur1)
 
   | Quitter -> etat, balle, joueur1;;
@@ -216,9 +233,55 @@ let rendu etat balle joueur1 joueur2 =
        ((balle.b_get_rayon()) * 2)
        ((balle.b_get_rayon()) * 2)
   end
-	
+    
   | Quitter -> ();; 
 
+
+
+(* Détecte une collision entre la balle et un joueur et, si c'est le cas,
+   dévie la balle en conséquence. *)
+let engin_collision_balle_joueur joueur1 joueur2 balle = 
+  (* Joueur 1*)
+  if balle.b_getvitessexf() < 0.0 &&
+    joueur1.jr_passe_rect (point_dans_rect (balle.b_getx()) (balle.b_gety())) then
+    balle.b_set_angle ((pi /. 4.) *. joueur1.jr_hauteur_relative (balle.b_gety()))
+  (* Joueur 2 *)
+  else if balle.b_getvitessexf() > 0.0 &&
+    joueur2.jr_passe_rect (point_dans_rect (balle.b_getx()) (balle.b_gety())) then
+    balle.b_set_angle (pi -. ((pi /. 4.) *. joueur2.jr_hauteur_relative (balle.b_gety())))
+  (* Aucune collision avec un joueur*)
+  else
+    balle;;
+
+
+
+(* Détecte une collision entre la balle et le mur du haut ou du bas et, si c'est le cas,
+   dévie la balle en conséquence. *)
+let engin_collision_mur balle = 
+  (* Mur du bas *)
+  if balle.b_gety() < 0 then
+    balle.b_applique_sur_direction (fun vx vy -> (vx, abs_float vy) )
+  (* Mur du haut *)
+  else if balle.b_gety() > hauteur_fenetre - 40 then
+    balle.b_applique_sur_direction (fun vx vy -> (vx, -. abs_float vy) )
+  (* Aucune collision avec un mur *)
+  else
+    balle;;
+
+
+
+(* Détecte un but marqué par un des joueur et, si c'est le cas, procède à la remise
+   au jeu. *)
+let engin_but balle =
+  (* But par joueur 1 *)
+  if balle.b_getx() > largeur_fenetre then 
+      (balle.b_set_pos (largeur_fenetre / 2) (hauteur_fenetre / 2)).b_set_angle pi
+  (* But par joueur 2 *)
+  else if balle.b_getx() < 0 then 
+    (balle.b_set_pos (largeur_fenetre / 2) (hauteur_fenetre / 2)).b_set_angle 0.
+  (* Aucun but *)
+  else 
+    balle;;
 
 
 
@@ -227,82 +290,28 @@ let engin estampille etat balle joueur1 joueur2 =
   | Splash | Pause | Quitter ->  balle, joueur1, joueur2
 
   | EnJeu -> let temps_ecoule = ((Unix.gettimeofday()) -. estampille) in 
-    (balle, joueur1, joueur2) 
 
-      (*Déplacement de la balle *)
-     |> (fun (balle, joueur1, joueur2) -> 
-	 ((balle.b_deplace temps_ecoule), joueur1, joueur2))
+             (* Déplacement de joueur2 *)
+             let dist_balle_j2 = (joueur2.jr_getyf()) -. (balle.b_getyf()) and
+                 step_j2 = temps_ecoule *. (joueur2.jr_getvitesse()) in
+             let joueur2 = if dist_balle_j2 > step_j2 then
+                 joueur2.jr_incyf (-. step_j2)
+               else if dist_balle_j2 < -. step_j2 then
+                 joueur2.jr_incyf step_j2
+               else
+                 joueur2 in
 
-     (* Déplacement de joueur2 *)
-     |> (fun (balle, joueur1, joueur2) ->  
-	 let dist_balle_j2 = (joueur2.jr_getyf()) -. (balle.b_getyf()) and
-	     step_j2 = temps_ecoule *. (joueur2.jr_getvitesse()) in
-	 (balle, joueur1,
-	  if dist_balle_j2 > step_j2 then
-	   joueur2.jr_incyf (-. step_j2)
-	 else if dist_balle_j2 < -. step_j2 then
-	   joueur2.jr_incyf step_j2
-	 else
-	   joueur2))
-
-    (* Collision balle/joueur1 (angles de rebond de de pi/4 à -pi/4) *)
-    |> (fun (balle, joueur1, joueur2) ->  
-    if balle.b_getvitessexf() < 0.0 &&
-      joueur1.jr_passe_rect (point_dans_rect (balle.b_getx()) (balle.b_gety())) then
-      (balle.b_set_angle ((pi /. 4.) *. joueur1.jr_hauteur_relative (balle.b_gety())),
-       joueur1,
-       joueur2)
-    else
-      (balle, joueur1, joueur2))
-
-    (* Collision balle/joueur2 (angles de rebond de 3pi/4 à 5pi/4)  *)
-    |> (fun (balle, joueur1, joueur2) ->  
-    if balle.b_getvitessexf() > 0.0 &&
-      joueur2.jr_passe_rect (point_dans_rect (balle.b_getx()) (balle.b_gety())) then
-      (balle.b_set_angle (pi -. ((pi /. 4.) *. joueur2.jr_hauteur_relative (balle.b_gety()))),
-       joueur1,
-       joueur2)
-    else
-      (balle, joueur1, joueur2))
-
-    (* Collision entre la balle et le mur du haut *)
-    |> (fun (balle, joueur1, joueur2) ->  
-	if balle.b_gety() > hauteur_fenetre - 40 then
-	  (balle.b_applique_sur_direction (fun vx vy -> (vx, -. abs_float vy) ),
-	   joueur1,
-	   joueur2)
-	else
-           (balle, joueur1, joueur2))
-
-    (* Collision entre la balle et le mur du bas *)
-    |> (fun (balle, joueur1, joueur2) ->  
-	if balle.b_gety() < 0 then
-	  (balle.b_applique_sur_direction (fun vx vy -> (vx, abs_float vy) ),
-	   joueur1,
-	   joueur2)
-	else
-           (balle, joueur1, joueur2))
-
-    (* But par joueur 1 *)
-    |> (fun (balle, joueur1, joueur2) ->  
-    if balle.b_getx() > largeur_fenetre then 
-      ((balle.b_set_pos (largeur_fenetre / 2) (hauteur_fenetre / 2)).b_set_angle pi,
-       joueur1,
-       joueur2)
-    else 
-      (balle, joueur1, joueur2))
-
-    (* But par joueur 2 *)
-    |> (fun (balle, joueur1, joueur2) ->  
-    if balle.b_getx() < 0 then 
-      ((balle.b_set_pos (largeur_fenetre / 2) (hauteur_fenetre / 2)).b_set_angle 0.,
-       joueur1,
-       joueur2)
-    else 
-      (balle, joueur1, joueur2))
+             (* Déplacement de la balle *)
+             let balle = (balle.b_deplace temps_ecoule)
+             (* Gestion des collisions de la balle *)
+             |> engin_collision_balle_joueur joueur1 joueur2
+             |> engin_collision_mur
+             |> engin_but in
+             
+             (balle, joueur1, joueur2)
 ;;
 
- 
+
 
 (**** Fonction principale ****)
 let main () = 
@@ -313,25 +322,29 @@ let main () =
     (* Boucle principale implémentée par une fonction récursive terminale *)
     let rec aux estampille etat balle joueur1 joueur2 =
       begin
-	(* Rendu *)
+        (* Rendu *)
         Graphics.clear_graph ();
-	rendu etat balle joueur1 joueur2;
+        rendu etat balle joueur1 joueur2;
         Graphics.synchronize();
 
-	(* Gestion d'entrée utilisateur *)
-	let (etat, balle, joueur1) = entree etat balle joueur1 in
-	
-	(* Mouvement de la balle, de l'adversaire et gestion des collisions*)
-	let (balle, joueur1, joueur2) = engin estampille etat balle joueur1 joueur2 in
+        (* Gestion d'entrée utilisateur *)
+        let (etat, balle, joueur1) = entree etat balle joueur1 in
+
+        (* Limiter le nombre de boucles par secondes *)
+        let temps_ecoule = (Unix.gettimeofday()) -. estampille in
+        if temps_ecoule < (1. /. maxfps) then minisleep ((1. /. maxfps) -. temps_ecoule);
+        
+        (* Mouvement de la balle, de l'adversaire et gestion des collisions*)
+        let (balle, joueur1, joueur2) = engin estampille etat balle joueur1 joueur2 in
 
         (* Appel récursif conditionnel à l'état du jeu *)
         if etat <> Quitter then aux (Unix.gettimeofday()) etat balle joueur1 joueur2;
       end in
     aux (Unix.gettimeofday()) 
-        Splash
-        ((batiBalle (float_of_int (largeur_fenetre/2))
-	            (float_of_int (hauteur_fenetre/2)) 0. 0.).b_set_angle 0.)
-        (batiJoueur (float_of_int 10) (float_of_int (hauteur_fenetre/2)))
-        (batiJoueur (float_of_int (largeur_fenetre - 40)) (float_of_int (hauteur_fenetre/2)));
+      Splash
+      ((batiBalle (float_of_int (largeur_fenetre/2))
+	      (float_of_int (hauteur_fenetre/2)) 0. 0.).b_set_angle 0.)
+      (batiJoueur (float_of_int 10) (float_of_int (hauteur_fenetre/2)))
+      (batiJoueur (float_of_int (largeur_fenetre - 40)) (float_of_int (hauteur_fenetre/2)));
     Graphics.close_graph();
   end;;
